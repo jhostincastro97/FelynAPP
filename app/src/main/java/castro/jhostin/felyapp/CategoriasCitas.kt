85706bf466457f20ebc2809cfdd154a49ae3ef95
Jhostin <jhostin.itson97@gmail.com>
2020-04-06 00:30:17 -0700

    Vista 5 terminada

--- app/src/main/java/castro/jhostin/felyapp/CategoriasCitas.kt
+++ app/src/main/java/castro/jhostin/felyapp/CategoriasCitas.kt
@@ -1,12 +1,21 @@
 package castro.jhostin.felyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_categorias_citas.*
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class CategoriasCitas : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_categorias_citas)
+
+        btn_Geriatria.setOnClickListener{
+            var intent = Intent(this,Geriatria::class.java)
+            startActivity(intent)
+        }
+
     }
 }
