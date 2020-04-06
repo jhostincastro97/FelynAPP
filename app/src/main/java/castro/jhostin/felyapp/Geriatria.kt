750084c9195612f17cd5a591f7b66bcceedfaa4b
Antonio23-Leyva <luisa.leyva@potros.itson.edu.mx>
2020-04-05 23:22:38 -0700

    Agregacion seccion geratria

---
+++ app/src/main/java/castro/jhostin/felyapp/Geriatria.kt
@@ -0,0 +1,12 @@
+package castro.jhostin.felyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Geriatria : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_geriatria)
+    }
+}
