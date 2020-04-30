9565cd53cc7b49a15bcceaf5b4cf99bfdfea5513
Antonio23-Leyva <luisa.leyva@potros.itson.edu.mx>
2020-04-30 12:29:14 -0700

    login google listo

--- app/src/main/java/castro/jhostin/felyapp/LoginActivity.kt
+++ app/src/main/java/castro/jhostin/felyapp/LoginActivity.kt
@@ -1,7 +1,9 @@
 package castro.jhostin.felyapp
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -10,30 +12,24 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
-import kotlinx.android.synthetic.main.activity_crear_usuario.*
 import kotlinx.android.synthetic.main.activity_login.*
 
 
+val RC_SIGN_IN = 123
+val CODE_LOGOUT = 321
 class  LoginActivity : AppCompatActivity() {
-    val RC_SIGN_IN = 123
-    val CODE_LOGOUT = 321
+
     lateinit var mGoogleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
         //Implementacion de sdk de google para el inicio de sesion
-
         // Configure sign-in to request the user's ID, email address, and basic
-// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-
-        // Configure sign-in to request the user's ID, email address, and basic
-// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build()
-
         // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
@@ -42,23 +38,45 @@ class  LoginActivity : AppCompatActivity() {
             startActivityForResult(signInIntent, RC_SIGN_IN)
 
         }
-
         //Funciones con intent
 
-        btn_Ingresar.setOnClickListener{
-            var intent = Intent(this,MenuActivity::class.java)
-            startActivity(intent)
-        }
 
         btn_CrearUsuario.setOnClickListener{
             var intent = Intent(this,CrearUsuario::class.java)
             startActivity(intent)
         }
 
-
-
-
+        btn_A_inicio.setOnClickListener{
+            var intent = Intent(this,Inicio::class.java)
+            startActivity(intent)
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        updateUI(account)
+    }
+
+
+//    fun onClick(v: View) {
+//        when (v.getId()) {
+//            R.id.btn_sign_in_button -> signIn()
+//        }
+//    }
+//
+//    private fun signIn() {
+//        val signInIntent = mGoogleSignInClient.signInIntent
+//        startActivityForResult(signInIntent, RC_SIGN_IN)
+//    }
+
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -75,23 +93,20 @@ class  LoginActivity : AppCompatActivity() {
 
     }
 
-    override fun onStart() {
-        super.onStart()
-
-        // Check for existing Google Sign In account, if the user is already signed in
-// the GoogleSignInAccount will be non-null.
-
-        // Check for existing Google Sign In account, if the user is already signed in
-// the GoogleSignInAccount will be non-null.
-        val account = GoogleSignIn.getLastSignedInAccount(this)
-        updateUI(account)
-    }
 
     private fun signOut() {
-        mGoogleSignInClient.signOut().addOnCompleteListener(this) {
+        mGoogleSignInClient.signOut()
+            .addOnCompleteListener(this) {
                 Toast.makeText(this, "A cerrado sesion",Toast.LENGTH_SHORT).show()
+//                revokeAccess()
             }
     }
+//    private fun signOut() {
+//        mGoogleSignInClient.signOut()
+//            .addOnCompleteListener(this,{
+//                Toast.makeText(this, "A cerrado sesion",Toast.LENGTH_SHORT).show()
+//            }
+//    }
 
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
         try {
@@ -103,19 +118,21 @@ class  LoginActivity : AppCompatActivity() {
         } catch (e: ApiException) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
-//            Log.w(FragmentActivity.TAG, "signInResult:failed code=" + e.statusCode)
+            Log.w("test_signin", "signInResult:failed code=" + e.statusCode)
             updateUI(null)
         }
     }
 
     private fun updateUI(account: GoogleSignInAccount?) {
+
         if (account != null){
            val intent = Intent(this, MenuActivity::class.java)
             intent.putExtra("name", account.displayName)
             intent.putExtra("email", account.email)
             Toast.makeText(this, "Sesion Iniciada",Toast.LENGTH_SHORT).show()
-            startActivityForResult(intent,CODE_LOGOUT)
+            startActivityForResult(intent, CODE_LOGOUT)
         }
     }
+
 
 }
--- app/src/main/java/castro/jhostin/felyapp/MenuActivity.kt
+++ app/src/main/java/castro/jhostin/felyapp/MenuActivity.kt
@@ -1,8 +1,10 @@
 package castro.jhostin.felyapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
@@ -14,14 +16,18 @@ class MenuActivity : AppCompatActivity() {
         val bundle = intent.extras
         if (bundle != null){
             val nombre = bundle.getString("name")
+            val correo = bundle.getString("email")
 
-            tv_nombre.setText(nombre)
+            tv_nombre.setText("!Bienvenido(a)¡\n"+nombre)
+            tv_correo.setText(correo)
         }
 
         btn_InfoGeneral.setOnClickListener{
             var intent = Intent(this,InformacionGeneral::class.java)
             startActivity(intent)
         }
+
+
 
         btn_CitasMedicas.setOnClickListener{
             var intent = Intent(this,CategoriasCitas::class.java)
@@ -33,9 +39,10 @@ class MenuActivity : AppCompatActivity() {
             startActivity(intent)
         }
         btn_cerrar_sesion.setOnClickListener{
+//           Toast.makeText(this, "Sesion Cerrada",Toast.LENGTH_SHORT).show()
             finish()
-            var intent = Intent(this, Inicio::class.java)
-            startActivity(intent)
+//            var intent = Intent(this, LoginActivity::class.java)
+//            startActivity(intent)
         }
 
 
